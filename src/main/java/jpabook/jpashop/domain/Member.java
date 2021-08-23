1606e3814f308f313276880acd0875db16c0117c
Yoonho Ahn <yoonho5684@gmail.com>
2021-08-23 23:30:58 +0900

    init

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order 필드에 있는 member에 매핑 된 것 -> 읽기전용
+    private List<Order> orders = new ArrayList<>();
+}
