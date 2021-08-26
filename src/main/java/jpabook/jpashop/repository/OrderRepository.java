338f9b2f7fdcd39955a288b9572864583dbf491a
Yoonho Ahn <yoonho5684@gmail.com>
2021-08-26 19:16:45 +0900

    chore: prepare order serach

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,5 +21,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch)
+//    검색 - 동적 쿼리 필요
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//        return em.createQuery("select o from Order o join o.member m" +
+//                " where o.status = :status" +
+//                " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(100)
+//                .getResultList();
+//    }
 }
