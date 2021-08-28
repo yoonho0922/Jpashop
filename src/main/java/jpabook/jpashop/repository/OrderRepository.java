62130460ce183a2874833c81fe573d529a2256e4
Yoonho Ahn <yoonho5684@gmail.com>
2021-08-28 14:47:42 +0900

    feat: implement order controller

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -21,14 +23,44 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    검색 - 동적 쿼리 필요
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        return em.createQuery("select o from Order o join o.member m" +
-//                " where o.status = :status" +
-//                " and m.name like :name", Order.class)
-//                .setParameter("status", orderSearch.getOrderStatus())
-//                .setParameter("name", orderSearch.getMemberName())
-//                .setMaxResults(100)
-//                .getResultList();
-//    }
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
 }
