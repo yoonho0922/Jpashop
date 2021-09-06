f04ae1bab4568acb72a4ecdb087365813708a87f
Yoonho Ahn <yoonho5684@gmail.com>
2021-09-06 22:57:05 +0900

    feat: member api

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,105 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController //Controller + ResponseBody
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /*
+        엔티티를 직접 노출하는 경우 (절대 사용 X)
+     */
+//    @PostMapping("/api/v1/members")
+//    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+//        Long id = memberService.join(member);
+//        return new CreateMemberResponse(id);
+//    }
+
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member updateMember = memberService.findOne(id);
+        return new UpdateMemberResponse(updateMember.getId(), updateMember.getName());
+    }
+
+    /*
+        엔티티를 직접 노출하는 경우 : 조인된 테이블도 json으로 가져오게됨
+        Member 필드에 @JsonIgnore를 사용하면 되지만, 유지보수 어려움
+     */
+//    @GetMapping("api/v1/members")
+//    public List<Member> membersV1(){
+//        return memberService.findMember();
+//    }
+
+    @GetMapping("api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMember();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size() ,collect);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+        private Long id;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
+}
